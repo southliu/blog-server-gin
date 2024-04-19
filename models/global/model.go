dd3330db922c83735fe54abfb8084ce58a3ac586
southliu <1275093225@qq.com>
2024-04-19 20:13:57 +0800

    fix: 初始化时间失败

--- models/global/model.go
+++ models/global/model.go
@@ -1,13 +1,15 @@
 package global
 
 import (
+	"database/sql"
+	"database/sql/driver"
 	"fmt"
 	"time"
 
 	"gorm.io/gorm"
 )
 
-type LocalTime time.Time
+type LocalTime sql.NullTime
 
 type GVA_MODEL struct {
 	ID uint64 `json:"id" gorm:"primarykey"` // 主键ID
@@ -25,6 +27,17 @@ type PAGE_MODEL struct {
 }
 
 func (t *LocalTime) MarshalJSON() ([]byte, error) {
-	tTime := time.Time(*t)
+	tTime := time.Time(t.Time)
 	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02 15:04:05"))), nil
+}
+
+func (n *LocalTime) Scan(value interface{}) error {
+	return (*sql.NullTime)(n).Scan(value)
+}
+
+func (n LocalTime) Value() (driver.Value, error) {
+	if !n.Valid {
+		return nil, nil
+	}
+	return n.Time, nil
 }
